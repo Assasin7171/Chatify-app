5fdca0c6ef10736ef9f99cf093482ae9640cd9ea
Assasin7171 <kbojanecki@gmail.com>
2025-12-11 13:04:37 +0100

    .

---
+++ backend/src/lib/utils.js
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+
+export function generateToken(userId, res) {
+    const token = jwt.sign({id: userId}, process.env.JWT_SECRET, {
+        expiresIn: "7d",
+    });
+    res.cookie("jwt", token, {
+        httpOnly: true,
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
+        sameSite: "strict", // CSRF protection
+        secure: process.env.NODE_ENV === "development" ? false : true,
+    });
+
+    return token;
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import {connectDB} from "./lib/db.js";
 
 dotenv.config();
 
@@ -13,16 +14,21 @@ const __dirname = path.resolve();
 //do ukrywania wartości aby nie były zakodowane w repozytorium na sztywno
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json()); //reqest body
+
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
 //make ready for deployment
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-  });
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    });
 }
 
-app.listen(PORT, () => console.log("Server is running on port " + PORT));
+app.listen(PORT, async () => {
+    console.log("Server is running on port " + PORT);
+    await connectDB();
+});
